7b3f3eaca6779e1eecea5a6efde62dfdc0caf661
Hafid1803 <127041144+Hafid1803@users.noreply.github.com>
2023-05-15 00:13:53 +0700

    TUBES_1302210129
    Penambahan buku baru ke dalam sistem dan pengurangan buku dari sistem

---
+++ KELP1_Hafid Naoya/Penghapusan Buku.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bookstore
+
+namespace BookstoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookId = 123; // ID buku yang akan dihapus
+            var bookstore = new Bookstore();
+            var isSuccess = bookstore.DeleteBook(bookId); // Panggil method untuk menghapus buku
+            if (isSuccess)
+            {
+                Console.WriteLine($"Buku dengan ID {bookId} berhasil dihapus dari sistem.");
+            }
+            else
+            {
+                Console.WriteLine($"Gagal menghapus buku dengan ID {bookId}.");
+            }
+        }
+    }
+}
---
+++ KELP1_Hafid Naoya/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+class Program
+{
+    // enum untuk state state machine
+    enum BookState
+    {
+        Title,
+        Author,
+        Year,
+        Confirmation,
+        Finish
+    }
+
+    static void Main(string[] args)
+    {
+        // inisialisasi objek buku baru
+        Book newBook = new Book();
+
+        // inisialisasi state awal state machine
+        BookState state = BookState.Title;
+
+        // state machine untuk mengatur input data buku
+        while (state != BookState.Finish)
+        {
+            switch (state)
+            {
+                case BookState.Title:
+                    Console.Write("Masukkan judul buku: ");
+                    newBook.Title = Console.ReadLine();
+                    state = BookState.Author;
+                    break;
+
+                case BookState.Author:
+                    Console.Write("Masukkan nama pengarang buku: ");
+                    newBook.Author = Console.ReadLine();
+                    state = BookState.Year;
+                    break;
+
+                case BookState.Year:
+                    Console.Write("Masukkan tahun terbit buku: ");
+                    int year = 0;
+                    if (int.TryParse(Console.ReadLine(), out year))
+                    {
+                        newBook.Year = year;
+                        state = BookState.Confirmation;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input tahun terbit buku tidak valid!");
+                    }
+                    break;
+
+                case BookState.Confirmation:
+                    Console.WriteLine("Apakah data buku sudah benar? (y/n)");
+                    string confirmation = Console.ReadLine();
+                    if (confirmation.ToLower() == "y")
+                    {
+                        state = BookState.Finish;
+                    }
+                    else if (confirmation.ToLower() == "n")
+                    {
+                        state = BookState.Title;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input tidak valid!");
+                    }
+                    break;
+            }
+        }
+
+        // menambahkan buku baru ke dalam sistem
+        Console.WriteLine("Buku baru berhasil ditambahkan ke dalam sistem:");
+        Console.WriteLine("Judul: " + newBook.Title);
+        Console.WriteLine("Pengarang: " + newBook.Author);
+        Console.WriteLine("Tahun terbit: " + newBook.Year);
+        Console.ReadLine();
+    }
+}
+
+// class untuk menyimpan data buku
+class Book
+{
+    public string Title;
+    public string Author;
+    public int Year;
+}
